3c9f5ebb44c97090921843bff3b984f60091dacb
Qiang Zeng <1357977299@qq.com>
2024-08-01 16:48:13 +0800

    remove uneccessary member

--- include/custom_alloc.h
+++ include/custom_alloc.h
@@ -6,10 +6,6 @@ template <typename T>
 class CustomAllocator {
 public:
     using value_type = T;
-    using pointer = T*;
-    using const_pointer = const T*;
-    using reference = T&;
-    using const_reference = const T&;
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
     CustomAllocator() = default;
@@ -29,11 +25,6 @@ public:
     size_type max_size() const noexcept {
         return std::numeric_limits<size_type>::max() / sizeof(T);
     }
-    template <typename U>
-    struct rebind {
-        using other = CustomAllocator<U>;
-    };
-
 private:
     template <typename U>
     friend class CustomAllocator;
